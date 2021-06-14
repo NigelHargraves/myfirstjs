5409efddfad45c52023490b2f0adc004cf185637
Nigel Hargraves <nigelhargraves@ymail.com>
2021-06-14 21:18:14 +0100

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,12 @@
 alert('Hello World!');
 console.log('secret message');
+document.write("a new line");
+let numberOfKittens = 5;
+let numberOfPuppies = 5;
+let numberOfPets = numberOfKittens + numberOfPuppies;
+console.log(numberOfKittens);
+document.write(numberOfKittens);
+console.log(numberOfPuppies);
+document.write(numberOfPuppies);
+console.log(numberOfPets);
+document.write(numberOfPets);
